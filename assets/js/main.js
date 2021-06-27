3e6ba5442a11e8e99a4a345905b019ee680096a9
VladimirJevtic <vjkblmb@gmail.com>
2021-06-27 19:45:06 +0200

    ready for changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,7 @@ function toggleSkills() {
     skillsContent[i].className = 'skills__content skills__close'
   }
   if (itemClass === 'skills__content skills__close') {
-    this.parentNode.className = 'skills__content skills_open'
+    this.parentNode.className = 'skills__content skills__open'
   }
 }
 
@@ -119,7 +119,7 @@ let swiperPortfolio = new Swiper('.portfolio__container', {
 });
 
 /*==================== TESTIMONIAL ====================*/
-let swiperTestimonial = new Swiper('.testimonial__container', {
+let swiperTestimonial = new Swiper('.testimonial_container', {
 
   loop: true,
   grabCursor: true,
@@ -141,6 +141,7 @@ let swiperTestimonial = new Swiper('.testimonial__container', {
   keyboard: true, */
 });
 
+
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
@@ -161,6 +162,7 @@ function scrollActive() {
 }
 window.addEventListener('scroll', scrollActive)
 
+
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
   const nav = document.getElementById('header')
@@ -169,13 +171,17 @@ function scrollHeader() {
 }
 window.addEventListener('scroll', scrollHeader)
 
+
+
 /*==================== SHOW SCROLL UP ====================*/
 function scrollUp() {
   const scrollUp = document.getElementById('scroll-up');
   // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  if (this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+
 
 /*==================== DARK LIGHT THEME ====================*/
 
